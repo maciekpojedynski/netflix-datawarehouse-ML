8c58f82410c04552af5cbf34dcbef60be95c3af6
Maciek Pojedyński <100421227+maciekpojedynski@users.noreply.github.com>
2025-11-23 22:48:45 +0100

    Update DDL_silver_logs_recommendation.sql

--- notebooks/DDL_silver_logs_recommendation.sql
+++ notebooks/DDL_silver_logs_recommendation.sql
@@ -4,7 +4,7 @@ CREATE TABLE silver.logs_recommendation(
 	recommendation_id NVARCHAR(255),
 	user_id NVARCHAR(255),
 	movie_id NVARCHAR(255),
-	recommendation_date NVARCHAR(255),
+	recommendation_date DATE,
 	recommendation_type NVARCHAR(255),
 	recommendation_score DECIMAL(15,3),
 	was_clicked NVARCHAR(50),
