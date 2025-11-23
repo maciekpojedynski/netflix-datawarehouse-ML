3615ce08b1e8affa7b49887f1b133b08fd982fb4
Maciek Pojedyński <100421227+maciekpojedynski@users.noreply.github.com>
2025-11-23 22:49:45 +0100

    Update load_silver_logs_recommendation.sql

--- notebooks/load_silver_logs_recommendation.sql
+++ notebooks/load_silver_logs_recommendation.sql
@@ -1,6 +1,6 @@
 TRUNCATE TABLE silver.logs_recommendation;
 	BULK INSERT silver.logs_recommendation
-	FROM 'C:\Users\Maciek\Desktop\netflixdb\script\silver\python\netflix_silver_layer_logs1.csv'
+	FROM 'C:\Users\Maciek\Desktop\netflixdb\script\silver\python\netflix_silver_layer_recomm_logs.csv'
 	WITH(
 		FIRST_ROW = 2,
 		FIELDTERMINATOR = ',',
