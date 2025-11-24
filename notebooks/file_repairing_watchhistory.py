42c569b7bb7a12b8745f52f2d85257fce028e60f
Maciek Pojedyński <100421227+maciekpojedynski@users.noreply.github.com>
2025-11-24 14:37:16 +0100

    Refactor file_repairing_watchhistory.py with comments

--- notebooks/file_repairing_watchhistory.py
+++ notebooks/file_repairing_watchhistory.py
@@ -2,20 +2,20 @@ import pandas as pd
 import re
 import io
 
-# Ścieżka do uszkodzonego pliku
+# Damaged file path
 file_path = r'databases/netflix_silver_layer_watch_history.csv' 
 
-# 1. Wczytaj cały plik jako jeden surowy string
+# 1. Loading data as one string
 with open(file_path, 'r', encoding='utf-8') as f:
     raw_data = f.read()
 
-# 2. Usuń niedrukowalne znaki kontrolne Unicode (np. BOM, ukryte spacje)
+# 2. Deleting invisibale chars 
 cleaned_data = re.sub(r'[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]', '', raw_data) 
 
-# 3. Wymuszenie standardowego terminatora wiersza
+# 3. Standard terminator
 cleaned_data = cleaned_data.replace('\r\n', '\n').replace('\r', '\n')
 
-# 4. Wczytaj oczyszczony string do DataFrame
+# 4. Load data into df
 df_watch_history = pd.read_csv(io.StringIO(cleaned_data), sep=',')
 
 print(df_watch_history.head())
@@ -27,4 +27,5 @@ df_watch_history.to_csv(
     index=False,
     sep=',',
     encoding='utf-8'
+
 )
