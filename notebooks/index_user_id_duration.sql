c74bc6535e1e8853fa6ce18938442f6d541903ad
Maciek Pojedyński <100421227+maciekpojedynski@users.noreply.github.com>
2025-11-27 00:02:49 +0100

    Update index_user_id_duration.sql

--- notebooks/index_user_id_duration.sql
+++ notebooks/index_user_id_duration.sql
@@ -1,3 +1,9 @@
 CREATE NONCLUSTERED INDEX NC_watchhistory_userid_duration
 ON silver.watch_history (user_id ASC)
 INCLUDE (watch_duration_minutes);
+
+CREATE NONCLUSTERED INDEX NC_reviews_reviewid
+ON bronze.reviews_numeric (review_id ASC);
+
+CREATE NONCLUSTERED INDEX NC_reviews_reviewid_string
+ON bronze.reviews_string (review_id, user_id ASC)
