19bd9dc834a16fb01791e91d4589dc632fa4b017
Maciek Pojedyński <100421227+maciekpojedynski@users.noreply.github.com>
2025-12-07 17:42:39 +0100

    Create SQL script to load search logs into gold schema

---
+++ notebooks/load_gold_searchlogs.sql
@@ -0,0 +1,37 @@
+INSERT INTO gold.search_logs(
+	search_id,
+	user_id,
+	device_type,
+	search_query,
+	search_duration_seconds,
+	search_date,
+	results_returned,
+	had_typo,
+	used_filters,
+	location_country,
+	is_clicked_result_position_missing,
+	is_search_duration_seconds_missing
+)
+SELECT
+	search_id,
+	user_id,
+	device_type,
+	search_query,
+	search_duration_seconds,
+	search_date,
+	results_returned,
+	CASE 
+		WHEN had_typo = 'True' THEN 1
+		ELSE 0
+	END AS had_typo,
+	CASE 
+		WHEN used_filters = 'True' THEN 1
+		ELSE 0
+	END AS used_filters,
+	CASE 
+		WHEN location_country = 'USA' THEN 'United States'
+		ELSE location_country
+	END AS location_country,
+	is_clicked_result_position_missing,
+	is_search_duration_seconds_missing
+FROM silver.search_logs;
