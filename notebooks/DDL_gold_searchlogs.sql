b592d28421051054429c56c2ddd1a13c2421f0e8
Maciek Pojedyński <100421227+maciekpojedynski@users.noreply.github.com>
2025-12-07 17:42:24 +0100

    Create DDL_gold_searchlogs.sql

---
+++ notebooks/DDL_gold_searchlogs.sql
@@ -0,0 +1,18 @@
+IF OBJECT_ID('gold.search_logs') IS NOT NULL
+	DROP TABLE gold.search_logs;
+CREATE TABLE gold.search_logs(
+	SearchlogsKey INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	search_id NVARCHAR(14),
+	user_id NVARCHAR(255),
+	device_type NVARCHAR(255),
+	search_query NVARCHAR(255),
+	search_duration_seconds DECIMAL(15,2),
+	search_date DATE,
+	results_returned INT,
+	had_typo BIT,
+	used_filters BIT,
+	location_country NVARCHAR(255),
+	is_clicked_result_position_missing BIT,
+	is_search_duration_seconds_missing BIT,
+	loaddate DATETIME DEFAULT GETDATE()
+)
