65eb7e2f7f2618e9b133b4c8aae2bdab9f583303
Maciek Pojedyński <100421227+maciekpojedynski@users.noreply.github.com>
2025-12-07 17:43:01 +0100

    Update and rename NC_gold_index.sql to NC_gold_indexs.sql

--- notebooks/NC_gold_indexs.sql
+++ notebooks/NC_gold_indexs.sql
@@ -1,3 +1,4 @@
 CREATE NONCLUSTERED INDEX NC_DimMovie_MovieID ON gold.movies (movie_id ASC);
 CREATE NONCLUSTERED INDEX NC_DimUsers_user_id ON gold.users (user_id ASC);
 CREATE NONCLUSTERED INDEX NC_DimReviews_review_id ON gold.reviews (review_id ASC);
+CREATE NONCLUSTERED INDEX NC_gold_FactSearchLogs ON gold.search_logs (search_id ASC);
