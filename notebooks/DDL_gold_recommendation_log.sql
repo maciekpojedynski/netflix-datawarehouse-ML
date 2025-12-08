0451717844bd55f61c547cc4f0f1e17ba9751b22
Maciek Pojedyński <100421227+maciekpojedynski@users.noreply.github.com>
2025-12-08 21:47:47 +0100

    Update DDL_gold_recommendation_log.sql

--- notebooks/DDL_gold_recommendation_log.sql
+++ notebooks/DDL_gold_recommendation_log.sql
@@ -1,8 +1,7 @@
 IF OBJECT_ID('gold.logs_recommendation','U') IS NOT NULL
 	DROP TABLE gold.logs_recommendation;
 CREATE TABLE gold.logs_recommendation(
-	Logsrecommendationkey INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	recommendation_id NVARCHAR(255),
+	recommendation_id NVARCHAR(255) NOT NULL PRIMARY KEY,
 	user_id NVARCHAR(255),
 	movie_id NVARCHAR(255),
 	recommendation_type NVARCHAR(255),
@@ -14,3 +13,4 @@ CREATE TABLE gold.logs_recommendation(
 	was_clicked NVARCHAR(6),
 	algorithm_version NVARCHAR(255),
 	loaddate DATE DEFAULT GETDATE()
+)
