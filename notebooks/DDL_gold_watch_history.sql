fd8ec37ec62969a2edf55a87f421e504e51b27af
Maciek Pojedyński <100421227+maciekpojedynski@users.noreply.github.com>
2025-12-08 21:15:33 +0100

    Create DDL_gold_watch_history.sql

---
+++ notebooks/DDL_gold_watch_history.sql
@@ -0,0 +1,20 @@
+IF OBJECT_ID('gold.watch_history','U') IS NOT NULL
+	DROP TABLE gold.watch_history;
+CREATE TABLE gold.watch_history(
+	sessionkey INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	user_id NVARCHAR(255),
+	movie_id NVARCHAR(255),
+	device_type NVARCHAR(255),
+	location_country NVARCHAR(255),
+	quality NVARCHAR(255),
+	action NVARCHAR(255),
+	watch_duration_minutes DECIMAL(15,2),
+	progress_percentage INT,
+	user_rating INT,
+	watched_more_then_once BIT,
+	is_download BIT,
+	is_progress_percentage_missing BIT,
+	is_user_rating_missing BIT,
+	is_watch_duration_minutes_missing BIT,
+	loaddate DATE DEFAULT GETDATE()
+)
