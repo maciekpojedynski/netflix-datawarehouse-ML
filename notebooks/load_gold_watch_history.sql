d2f8ac32680a7eb0e4e45b0412a08311cdbb9274
Maciek Pojedyński <100421227+maciekpojedynski@users.noreply.github.com>
2025-12-09 13:27:24 +0100

    Update load_gold_watch_history.sql

--- notebooks/load_gold_watch_history.sql
+++ notebooks/load_gold_watch_history.sql
@@ -10,10 +10,7 @@ INSERT INTO gold.watch_history(
 	progress_percentage,
 	user_rating,
 	watched_more_then_once,
-	is_download,
-	is_progress_percentage_missing,
-	is_user_rating_missing,
-	is_watch_duration_minutes_missing
+	is_download
 )
 SELECT 
 	session_id,
@@ -30,9 +27,6 @@ SELECT
 	CAST(SUBSTRING(CAST(progress_percentage AS NVARCHAR),1,LEN(progress_percentage)-5) AS INT),
 	CAST(user_rating AS INT),
 	watched_more_then_once,
-	is_dowload AS is_download,
-	is_progress_percentage_missing,
-	is_user_rating_missing,
-	is_watch_duration_minutes_missing
+	is_dowload AS is_download
 FROM silver.watch_history
 WHERE licznik = 1;
